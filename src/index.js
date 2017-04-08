32593042cdc78a8bb846c460042e3467d21e6bde
Simon Dell <simon@expressionworks.co.uk>
2017-04-08 18:32:58 +0100

    Multi-tap booms
    They leave a stray gain hanging around, which I'm not sure how to remedy

--- src/index.js
+++ src/index.js
@@ -9,12 +9,11 @@ masterAmp.gain.value = 0.3;
 masterAmp.connect(con.destination);
 
 // boomOsc
-var booming = false;
-var boomOsc1, boomOsc2, boomOsc3;
-var boomAmp = con.createGain();
-boomAmp.connect(masterAmp);
-
 function boom() {
+	var booming = false;
+	var boomOsc1, boomOsc2, boomOsc3;
+	var boomAmp = con.createGain();
+
 	boomOsc1 = con.createOscillator();
 	boomOsc2 = con.createOscillator();
 	boomOsc3 = con.createOscillator();
@@ -26,30 +25,50 @@ function boom() {
 	boomOsc1.connect(boomAmp);
 	boomOsc2.connect(boomAmp);
 	boomOsc3.connect(boomAmp);
-	boomAmp.gain.value = 0.3;
+	boomAmp.gain.setValueAtTime(0.3, con.currentTime);
 
+	boomAmp.connect(masterAmp);
 	boomOsc1.start();
 	boomOsc2.start();
 	boomOsc3.start();
 	booming = true;
+
+	function boomTiltChanged (data) {
+		if(!booming) return;
+
+		// z controls freq (steer with your phone)
+		boomOsc1.frequency.value = data.z * 0.98;
+		boomOsc2.frequency.value = data.z * 1.01;
+		boomOsc3.frequency.value = data.z * 0.99;
+
+		// x controls level (rotate around charger axis)
+		boomAmp.gain.setValueAtTime((data.x + 1) / 4, con.currentTime);
+	}
+
+	boomTilt.on('*', boomTiltChanged);
+
+	return function endBoom () {
+		booming = false;
+
+		var now = con.currentTime;
+		boomAmp.gain.linearRampToValueAtTime(0, now + 0.75);
+		// boomAmp.gain.setTargetAtTime(0, 0.5, 0.5);
+		boomOsc1.stop(con.currentTime + 0.76);
+		boomOsc2.stop(con.currentTime + 0.76);
+		boomOsc3.stop(con.currentTime + 0.76);
+		setTimeout(function () { 
+			boomAmp.disconnect(masterAmp);
+			boomAmp = null;
+		}, 1000 );
+	}
 }
 
-function endBoom () {
-	booming = false;
-	boomAmp.gain.linearRampToValueAtTime(0, con.currentTime + 0.75);
-	// boomAmp.gain.setTargetAtTime(0, 0.5, 0.5);
-	boomOsc1.stop(con.currentTime + 1.5);
-	boomOsc2.stop(con.currentTime + 1.5);
-	boomOsc3.stop(con.currentTime + 1.5);
-}
 
 
 //
 // controls
 //
 nx.onload = function () {
-	boomTilt.on('*', tilt1Changed);
-
 	boomButton.on('*', button1Changed);
 	boomButton.mode = 'toggle';
 
@@ -58,19 +77,8 @@ nx.onload = function () {
 	nx.colorize('accent', '#FF0000');
 };
 
-function tilt1Changed(data) {
-	if(!booming) return;
-
-	// z controls freq (steer with your phone)
-  boomOsc1.frequency.value = data.z * 0.98;
-  boomOsc2.frequency.value = data.z * 1.01;
-  boomOsc3.frequency.value = data.z * 0.99;
-
-	// x controls level (rotate around charger axis)
-	boomAmp.gain.value = (data.x + 1) / 4;
-}
-
+var booms = [];
 function button1Changed (data) {
-	if(!!data.press) boom();
-	else endBoom();
+	if(!!data.press) booms.push(boom());
+	else booms.shift()();
 };
