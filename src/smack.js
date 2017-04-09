4c67f81a1113468651c6e359cff396347bf59e61
Simon Dell <simon@expressionworks.co.uk>
2017-04-09 13:09:30 +0100

    Adding a smack/snare thing

--- src/smack.js
+++ src/smack.js
@@ -1,22 +1,41 @@
+// make white noise sample
+var bufferSize = 2 * con.sampleRate;
+var noiseBuffer = con.createBuffer(1, bufferSize, con.sampleRate);
+var output = noiseBuffer.getChannelData(0);
+for (var i = 0; i < bufferSize; i++) {
+    output[i] = Math.random() * 2 - 1;
+}
+
+
 function Smack (data) {
-	var smackSource = null;
-	var boomAmp = con.createGain();
+	var smackSource = con.createBufferSource();
+	smackSource.buffer = noiseBuffer;
+	smackSource.loop = true;
 
-	boomOsc1 = con.createOscillator();
+	var smackAmp = con.createGain();
+	smackAmp.gain.setValueAtTime(0.3, con.currentTime);
 
-	return function endSmack () {}
+	smackSource.connect(smackAmp);
+	smackAmp.connect(masterAmp);
+
+	smackSource.start(0);
+	var now = con.currentTime;
+	var smackDecay = 0.3;
+	smackAmp.gain.linearRampToValueAtTime(0, now + smackDecay);
+
+	return function endSmack () { }
 }
 
 var smacks = [];
 function smackPadChanged (data) {
-	if(!!data.press) booms.push(boom());
-	else booms.shift()();
+	if(!!data.press) smacks.push(Smack());
+	else smacks.shift()();
 };
 
 
 function setupSmack () {
-	smackButton.on('*', button1Changed);
-	smackButton.mode = 'impulse';
+	smackPad.on('*', smackPadChanged);
+	smackPad.mode = 'impulse';
 	// smackDecayDial.on('*', smackDecayChanged);
 	// smackDecayDial.set({value: smackDecay});
 }
