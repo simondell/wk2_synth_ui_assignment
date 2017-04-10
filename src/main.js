2779fa27917cafbf9333e3bf378e27249add48df
Simon Dell <simon@expressionworks.co.uk>
2017-04-10 01:32:31 +0100

    Tired. Settled on two sounds
    Tidied layout

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
+document.onscroll = function () {};
+
 nx.onload = function () {
-	nx.colorize('border', '#990000');
-	nx.colorize('fill', '#660000');
-	nx.colorize('accent', '#FF0000');
+	nx.colorize('border', '#000000');
+	nx.colorize('fill', '#ff6666');
+	nx.colorize('accent', '#000000');
 
 	setupBoom();
 	setupSmack();
+	setupBip();
 };
 
 var ac = window.AudioContext || window.webkitAudioContext;
